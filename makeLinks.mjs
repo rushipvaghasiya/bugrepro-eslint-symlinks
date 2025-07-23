ef04369a86f5162c630ea12aacde6fc9dc13150e
Euan Robertson <euan.robertson@mre.medical.canon>
2025-07-23 11:49:10 +0100

    Initial Commit

---
+++ makeLinks.mjs
@@ -0,0 +1,8 @@
+import fs from 'fs-extra';
+import path from 'node:path';
+
+fs.ensureSymlinkSync(
+  path.join(process.cwd(), '/link-src'),
+  path.join(process.cwd(), '/src/link-src'),
+  'dir'
+);

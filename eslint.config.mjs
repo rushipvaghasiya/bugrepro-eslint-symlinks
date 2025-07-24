f85a92e0092acebcbb3d2d0b219241f18711844f
Euan Robertson <euan.robertson@mre.medical.canon>
2025-07-24 13:19:49 +0100

    Reworked example

--- eslint.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'eslint/config';
-
-export default defineConfig([
-  {
-    ignores: ['.node_modules']
-  }
-])
---
+++ xxxeslint.config.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    ignores: [
+      "link-src/**/*.js"
+    ]
+  },
+    {
+        rules: {
+            "no-console": "error"
+        }
+    }
+];
